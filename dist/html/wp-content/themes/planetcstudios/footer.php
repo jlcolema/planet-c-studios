0c538e458d6c6c06ff96c2b73a5f1b4dde4f144a
Joshua Coleman <jlcolema@me.com>
2020-10-16 13:43:22 -0500

    Refine markup and remove what is not needed.

--- dist/html/wp-content/themes/planetcstudios/footer.php
+++ dist/html/wp-content/themes/planetcstudios/footer.php
@@ -12,64 +12,47 @@
  */
 
 ?>
-			<footer id="site-footer" role="contentinfo" class="header-footer-group">
 
-				<div class="section-inner">
+		</div>
 
-					<div class="footer-credits">
+	</main>
 
-						<p class="footer-copyright">&copy;
-							<?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://www.php.net/date */
-								_x( 'Y', 'copyright date format', 'planetcstudios' )
-							);
-							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						</p><!-- .footer-copyright -->
+	<footer role="contentinfo" class="footer">
 
-						<p class="powered-by-wordpress">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'planetcstudios' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'planetcstudios' ); ?>
-							</a>
-						</p><!-- .powered-by-wordpress -->
+		<div class="wrap footer__wrap">
 
-					</div><!-- .footer-credits -->
+			<div class="logo logo--secondary">
 
-					<a class="to-the-top" href="#site-header">
-						<span class="to-the-top-long">
-							<?php
-							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'To the top %s', 'planetcstudios' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-long -->
-						<span class="to-the-top-short">
-							<?php
-							/* translators: %s: HTML character for up arrow. */
-							printf( __( 'Up %s', 'planetcstudios' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-short -->
-					</a><!-- .to-the-top -->
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo__link">
 
-				</div><!-- .section-inner -->
+					<?php bloginfo( 'name' ); ?>
 
-			</footer><!-- #site-footer -->
+				</a>
 
-		<!-- <script src="js/vencor/modernizr-3.11.3.min.js"></script> -->
-		<!-- <script src="js/app.js"></script> -->
+			</div>
 
-		<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+			<p class="copyright">Copyright &copy; 2001-<?php echo date_i18n( _x( 'Y', 'copyright date format', 'planetcstudios' ) ); ?> <?php bloginfo( 'name' ); ?> &ndash; All Rights Reserved</p>
 
-		<!-- <script>
+		</div>
 
-			window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    		ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+	</footer>
 
-		</script> -->
+	<!-- <script src="js/vencor/modernizr-3.5.0.min.js"></script> -->
+	<!-- <script src="js/functions.js"></script> -->
 
-		<!-- <script src="https://www.google-analytics.com/analytics.js" async></script> -->
+	<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 
-		<?php wp_footer(); ?>
+	<!-- <script>
 
-	</body>
+		window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+		ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+
+	</script> -->
+
+	<!-- <script src="https://www.google-analytics.com/analytics.js" async></script> -->
+
+	<?php wp_footer(); ?>
+
+</body>
+
 </html>
