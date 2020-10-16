5820f6016370074c8823b43628902b9f79bf3670
Joshua Coleman <jlcolema@me.com>
2020-10-16 13:54:55 -0500

    Add starter markup for sections.

--- dist/html/wp-content/themes/planetcstudios/page-home.php
+++ dist/html/wp-content/themes/planetcstudios/page-home.php
@@ -17,4 +17,64 @@
 
 	<h1><?php the_title(); ?></h1>
 
+	<!-- Section: Welcome -->
+
+	<section role="" class="section section__welcome welcome">
+
+		<div class="inner-wrap section__inner-wrap welcome__inner-wrap">
+
+			<!-- Content -->
+	
+		</div>
+
+	</section>
+
+	<!-- Section: Clients -->
+
+	<section role="" class="section section__clients clients">
+
+		<div class="inner-wrap section__inner-wrap clients__inner-wrap">
+
+			<!-- Content -->
+
+		</div>
+
+	</section>
+
+	<!-- Section: Work -->
+
+	<section role="" class="section section__work work">
+
+		<div class="inner-wrap section__inner-wrap work__inner-wrap">
+
+			<!-- Content -->
+
+		</div>
+
+	</section>
+
+	<!-- Section: About -->
+
+	<section role="" clas="section section__about about">
+
+		<div class="inner-wrap section__inner-wrap about__inner-wrap">
+
+			<!-- Content -->
+
+		</div>
+
+	</section>
+
+	<!-- Section: Contact -->
+
+	<section role="" class="section section__contact contact">
+
+		<div class="inner-wrap section__inner-wrap contact__inner-wrap">
+
+			<!-- Content -->
+
+		</div>
+
+	</section>
+
 <?php get_footer(); ?>
