31cd83b6918956b1c72d599bbfbab26a624aaea9
Joshua Coleman <jlcolema@me.com>
2020-10-16 13:07:13 -0500

    Add starter tags, elements, and scripts.

--- dist/html/wp-content/themes/planetcstudios/header.php
+++ dist/html/wp-content/themes/planetcstudios/header.php
@@ -9,179 +9,198 @@
  * @since Planet C Studios 1.0
  */
 
-?><!DOCTYPE html>
+?>
+
+<!doctype html>
 
 <html class="no-js" <?php language_attributes(); ?>>
 
-	<head>
+<head>
 
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 
-		<link rel="profile" href="https://gmpg.org/xfn/11">
+	<!-- <title>Title</title> -->
 
-		<?php wp_head(); ?>
+	<!-- <meta name="description" content=""> -->
 
-	</head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<body <?php body_class(); ?>>
+	<!-- <meta property="og:title" content=""> -->
+	<!-- <meta property="og:type" content=""> -->
+	<!-- <meta property="og:url" content=""> -->
+	<!-- <meta property="og:image" content=""> -->
 
-		<?php
-		wp_body_open();
-		?>
+	<!-- <link rel="manifest" href="site.webmanifest"> -->
+	<!-- <link rel="apple-touch-icon" href="icon.png"> -->
+	<!-- Place favicon.ico in the root directory -->
 
-		<header id="site-header" class="header-footer-group" role="banner">
+	<!-- <link rel="stylesheet" href="css/normalize.css"> -->
+	<!-- <link rel="stylesheet" href="css/styles.css"> -->
 
-			<div class="header-inner section-inner">
+	<meta name="theme-color" content="#fafafa">
 
-				<div class="header-titles-wrapper">
+	<?php wp_head(); ?>
 
-					<?php
+</head>
 
-					// Check whether the header search is activated in the customizer.
-					$enable_header_search = get_theme_mod( 'enable_header_search', true );
+<body <?php body_class(); ?>>
 
-					if ( true === $enable_header_search ) {
+	<?php
+	wp_body_open();
+	?>
 
-						?>
+	<header id="site-header" class="header-footer-group" role="banner">
 
-						<button class="toggle search-toggle mobile-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
-							<span class="toggle-inner">
-								<span class="toggle-icon">
-									<?php planetcstudios_the_theme_svg( 'search' ); ?>
-								</span>
-								<span class="toggle-text"><?php _e( 'Search', 'planetcstudios' ); ?></span>
-							</span>
-						</button><!-- .search-toggle -->
+		<div class="header-inner section-inner">
 
-					<?php } ?>
+			<div class="header-titles-wrapper">
 
-					<div class="header-titles">
+				<?php
 
-						<?php
-							// Site title or logo.
-							planetcstudios_site_logo();
+				// Check whether the header search is activated in the customizer.
+				$enable_header_search = get_theme_mod( 'enable_header_search', true );
 
-							// Site description.
-							planetcstudios_site_description();
-						?>
+				if ( true === $enable_header_search ) {
 
-					</div><!-- .header-titles -->
+					?>
 
-					<button class="toggle nav-toggle mobile-nav-toggle" data-toggle-target=".menu-modal"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+					<button class="toggle search-toggle mobile-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
 						<span class="toggle-inner">
 							<span class="toggle-icon">
-								<?php planetcstudios_the_theme_svg( 'ellipsis' ); ?>
+								<?php planetcstudios_the_theme_svg( 'search' ); ?>
 							</span>
-							<span class="toggle-text"><?php _e( 'Menu', 'planetcstudios' ); ?></span>
+							<span class="toggle-text"><?php _e( 'Search', 'planetcstudios' ); ?></span>
 						</span>
-					</button><!-- .nav-toggle -->
+					</button><!-- .search-toggle -->
 
-				</div><!-- .header-titles-wrapper -->
+				<?php } ?>
 
-				<div class="header-navigation-wrapper">
+				<div class="header-titles">
 
 					<?php
-					if ( has_nav_menu( 'primary' ) || ! has_nav_menu( 'expanded' ) ) {
+						// Site title or logo.
+						planetcstudios_site_logo();
+
+						// Site description.
+						planetcstudios_site_description();
+					?>
+
+				</div><!-- .header-titles -->
+
+				<button class="toggle nav-toggle mobile-nav-toggle" data-toggle-target=".menu-modal"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+					<span class="toggle-inner">
+						<span class="toggle-icon">
+							<?php planetcstudios_the_theme_svg( 'ellipsis' ); ?>
+						</span>
+						<span class="toggle-text"><?php _e( 'Menu', 'planetcstudios' ); ?></span>
+					</span>
+				</button><!-- .nav-toggle -->
+
+			</div><!-- .header-titles-wrapper -->
+
+			<div class="header-navigation-wrapper">
+
+				<?php
+				if ( has_nav_menu( 'primary' ) || ! has_nav_menu( 'expanded' ) ) {
+					?>
+
+						<nav class="primary-menu-wrapper" aria-label="<?php esc_attr_e( 'Horizontal', 'planetcstudios' ); ?>" role="navigation">
+
+							<ul class="primary-menu reset-list-style">
+
+							<?php
+							if ( has_nav_menu( 'primary' ) ) {
+
+								wp_nav_menu(
+									array(
+										'container'  => '',
+										'items_wrap' => '%3$s',
+										'theme_location' => 'primary',
+									)
+								);
+
+							} elseif ( ! has_nav_menu( 'expanded' ) ) {
+
+								wp_list_pages(
+									array(
+										'match_menu_classes' => true,
+										'show_sub_menu_icons' => true,
+										'title_li' => false,
+										'walker'   => new PlanetCStudios_Walker_Page(),
+									)
+								);
+
+							}
+							?>
+
+							</ul>
+
+						</nav><!-- .primary-menu-wrapper -->
+
+					<?php
+				}
+
+				if ( true === $enable_header_search || has_nav_menu( 'expanded' ) ) {
+					?>
+
+					<div class="header-toggles hide-no-js">
+
+					<?php
+					if ( has_nav_menu( 'expanded' ) ) {
 						?>
 
-							<nav class="primary-menu-wrapper" aria-label="<?php esc_attr_e( 'Horizontal', 'planetcstudios' ); ?>" role="navigation">
+						<div class="toggle-wrapper nav-toggle-wrapper has-expanded-menu">
 
-								<ul class="primary-menu reset-list-style">
+							<button class="toggle nav-toggle desktop-nav-toggle" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+								<span class="toggle-inner">
+									<span class="toggle-text"><?php _e( 'Menu', 'planetcstudios' ); ?></span>
+									<span class="toggle-icon">
+										<?php planetcstudios_the_theme_svg( 'ellipsis' ); ?>
+									</span>
+								</span>
+							</button><!-- .nav-toggle -->
 
-								<?php
-								if ( has_nav_menu( 'primary' ) ) {
-
-									wp_nav_menu(
-										array(
-											'container'  => '',
-											'items_wrap' => '%3$s',
-											'theme_location' => 'primary',
-										)
-									);
-
-								} elseif ( ! has_nav_menu( 'expanded' ) ) {
-
-									wp_list_pages(
-										array(
-											'match_menu_classes' => true,
-											'show_sub_menu_icons' => true,
-											'title_li' => false,
-											'walker'   => new PlanetCStudios_Walker_Page(),
-										)
-									);
-
-								}
-								?>
-
-								</ul>
-
-							</nav><!-- .primary-menu-wrapper -->
+						</div><!-- .nav-toggle-wrapper -->
 
 						<?php
 					}
 
-					if ( true === $enable_header_search || has_nav_menu( 'expanded' ) ) {
+					if ( true === $enable_header_search ) {
 						?>
 
-						<div class="header-toggles hide-no-js">
+						<div class="toggle-wrapper search-toggle-wrapper">
 
-						<?php
-						if ( has_nav_menu( 'expanded' ) ) {
-							?>
+							<button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
+								<span class="toggle-inner">
+									<?php planetcstudios_the_theme_svg( 'search' ); ?>
+									<span class="toggle-text"><?php _e( 'Search', 'planetcstudios' ); ?></span>
+								</span>
+							</button><!-- .search-toggle -->
 
-							<div class="toggle-wrapper nav-toggle-wrapper has-expanded-menu">
+						</div>
 
-								<button class="toggle nav-toggle desktop-nav-toggle" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
-									<span class="toggle-inner">
-										<span class="toggle-text"><?php _e( 'Menu', 'planetcstudios' ); ?></span>
-										<span class="toggle-icon">
-											<?php planetcstudios_the_theme_svg( 'ellipsis' ); ?>
-										</span>
-									</span>
-								</button><!-- .nav-toggle -->
-
-							</div><!-- .nav-toggle-wrapper -->
-
-							<?php
-						}
-
-						if ( true === $enable_header_search ) {
-							?>
-
-							<div class="toggle-wrapper search-toggle-wrapper">
-
-								<button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
-									<span class="toggle-inner">
-										<?php planetcstudios_the_theme_svg( 'search' ); ?>
-										<span class="toggle-text"><?php _e( 'Search', 'planetcstudios' ); ?></span>
-									</span>
-								</button><!-- .search-toggle -->
-
-							</div>
-
-							<?php
-						}
-						?>
-
-						</div><!-- .header-toggles -->
 						<?php
 					}
 					?>
 
-				</div><!-- .header-navigation-wrapper -->
+					</div><!-- .header-toggles -->
+					<?php
+				}
+				?>
 
-			</div><!-- .header-inner -->
+			</div><!-- .header-navigation-wrapper -->
 
-			<?php
-			// Output the search modal (if it is activated in the customizer).
-			if ( true === $enable_header_search ) {
-				get_template_part( 'template-parts/modal-search' );
-			}
-			?>
-
-		</header><!-- #site-header -->
+		</div><!-- .header-inner -->
 
 		<?php
-		// Output the menu modal.
-		get_template_part( 'template-parts/modal-menu' );
+		// Output the search modal (if it is activated in the customizer).
+		if ( true === $enable_header_search ) {
+			get_template_part( 'template-parts/modal-search' );
+		}
+		?>
+
+	</header><!-- #site-header -->
+
+	<?php
+	// Output the menu modal.
+	get_template_part( 'template-parts/modal-menu' );
