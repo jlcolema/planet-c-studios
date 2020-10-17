44481309c88a3b964823969dc350a155dbbb8483
Joshua Coleman <jlcolema@me.com>
2020-10-17 17:31:49 -0500

    Add starter markup.

---
+++ dist/html/wp-content/themes/planetcstudios/single-project.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Planet_C_Studios
+ * @since Planet C Studios 1.0
+ */
+
+?>
+
+<?php get_header(); ?>
+
+	<h1>Project</h1>
+
+<?php get_footer(); ?>
