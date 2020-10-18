5223f9308960bef11d8622e7a75ceda61734088d
Joshua Coleman <jlcolema@me.com>
2020-10-18 14:50:00 -0500

    Add starter markup for 404 page.

--- dist/html/wp-content/themes/planetcstudios/404.php
+++ dist/html/wp-content/themes/planetcstudios/404.php
@@ -6,31 +6,10 @@
  * @subpackage Planet_C_Studios
  * @since Planet C Studios 1.0
  */
-
-get_header();
 ?>
 
-<main id="site-content" role="main">
+<?php get_header(); ?>
 
-	<div class="section-inner thin error404-content">
+	<h1 class="">404</h1>
 
-		<h1 class="entry-title"><?php _e( 'Page Not Found', 'planetcstudios' ); ?></h1>
-
-		<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'planetcstudios' ); ?></p></div>
-
-		<?php
-		get_search_form(
-			array(
-				'label' => __( '404 not found', 'planetcstudios' ),
-			)
-		);
-		?>
-
-	</div><!-- .section-inner -->
-
-</main><!-- #site-content -->
-
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
-
-<?php
-get_footer();
+<?php get_footer(); ?>
