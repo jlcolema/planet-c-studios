ac816a85e61c366b48364816146c2443fd40450b
Joshua Coleman <jlcolema@me.com>
2020-10-18 14:05:40 -0500

    Add markup and placeholder content for a proposal entry.

---
+++ dist/html/wp-content/themes/planetcstudios/single-proposal.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Planet_C_Studios
+ * @since Planet C Studios 1.0
+ */
+
+?>
+
+<?php get_header(); ?>
+
+	<div class="projects">
+
+		<div class="projects__nav">
+
+			<h1 class="projects__header">Projects (for Proposals)</h1>
+
+			<select name="projects-filter" id="" class="projects__filter">
+
+				<option value="">See All</option>
+				<option value="animation">Animation</option>
+				<option value="branding">Branding</option>
+				<option value="comedy">Comedy</option>
+				<option value="concept">Concept</option>
+				<option value="documentary">Documentary</option>
+				<option value="educational">Educational</option>
+				<option value="graphics">Graphics</option>
+				<option value="horror">Horror</option>
+				<option value="montage">Montage</option>
+				<option value="music-video">Music Video</option>
+				<option value="production">Production</option>
+				<option value="projection-mapping">Projection Mapping</option>
+				<option value="promo">Promo</option>
+				<option value="scripted">Scripted</option>
+				<option value="short">Short</option>
+				<option value="special-venue">Special Venue</option>
+				<option value="star-talent">Star Talent</option>
+				<option value="television">Television</option>
+				<option value="visual-effects">Visual Effects</option>
+
+			</select>
+
+		</div>
+
+		<ul class="projects__list">
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project"width="200" height="300" decoding="async" loading="lazy"  class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+			<li class="project__item project__item--category-animation">
+
+				<a href="/work/project-01/" class="project__link">
+
+					<div class="project__cover">
+
+						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
+
+					</div>
+
+					<h3 class="project__title">Title of Project</h3>
+
+				</a>
+
+			</li>
+
+		</ul>
+
+	</div>
+
+<?php get_footer(); ?>
