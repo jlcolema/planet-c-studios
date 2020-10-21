9bdedc56af0bed365e1147abb6b7460bed9af315
Joshua Coleman <jlcolema@me.com>
2020-10-21 09:15:59 -0500

    Disable support for adding custom widget areas. This is just commented out in case it is needed in the future.

--- dist/html/wp-content/themes/planetcstudios/functions.php
+++ dist/html/wp-content/themes/planetcstudios/functions.php
@@ -185,41 +185,61 @@ add_action( 'init', 'planetcstudios_menus' );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function planetcstudios_sidebar_registration() {
+
+// function planetcstudios_sidebar_registration() {
 
 	// Arguments used in all register_sidebar() calls.
-	$shared_args = array(
-		'before_title'  => '<h2 class="widget-title subheading heading-size-3">',
-		'after_title'   => '</h2>',
-		'before_widget' => '<div class="widget %2$s"><div class="widget-content">',
-		'after_widget'  => '</div></div>',
-	);
+
+	// $shared_args = array(
+
+		// 'before_title'  => '<h2 class="widget-title subheading heading-size-3">',
+		// 'after_title'   => '</h2>',
+		// 'before_widget' => '<div class="widget %2$s"><div class="widget-content">',
+		// 'after_widget'  => '</div></div>',
+
+	// );
 
 	// Footer #1.
-	register_sidebar(
-		array_merge(
-			$shared_args,
-			array(
-				'name'        => __( 'Footer #1', 'planetcstudios' ),
-				'id'          => 'sidebar-1',
-				'description' => __( 'Widgets in this area will be displayed in the first column in the footer.', 'planetcstudios' ),
-			)
-		)
-	);
+
+	// register_sidebar(
+
+		// array_merge(
+
+			// $shared_args,
+
+			// array(
+
+				// 'name'        => __( 'Footer #1', 'planetcstudios' ),
+				// 'id'          => 'sidebar-1',
+				// 'description' => __( 'Widgets in this area will be displayed in the first column in the footer.', 'planetcstudios' ),
+
+			// )
+
+		// )
+
+	// );
 
 	// Footer #2.
-	register_sidebar(
-		array_merge(
-			$shared_args,
-			array(
-				'name'        => __( 'Footer #2', 'planetcstudios' ),
-				'id'          => 'sidebar-2',
-				'description' => __( 'Widgets in this area will be displayed in the second column in the footer.', 'planetcstudios' ),
-			)
-		)
-	);
 
-}
+	// register_sidebar(
+
+		// array_merge(
+
+			// $shared_args,
+
+			// array(
+
+				// 'name'        => __( 'Footer #2', 'planetcstudios' ),
+				// 'id'          => 'sidebar-2',
+				// 'description' => __( 'Widgets in this area will be displayed in the second column in the footer.', 'planetcstudios' ),
+
+			// )
+
+		// )
+
+	// );
+
+// }
 
 add_action( 'widgets_init', 'planetcstudios_sidebar_registration' );
 
