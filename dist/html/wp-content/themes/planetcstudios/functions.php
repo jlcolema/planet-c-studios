f34fb57053016351075f6d189754e75a8b7c90aa
Joshua Coleman <jlcolema@me.com>
2020-10-21 09:23:10 -0500

    Disable support for displaying non-latin styles within the classic editor. This is commented out in case it is needed in the future.

--- dist/html/wp-content/themes/planetcstudios/functions.php
+++ dist/html/wp-content/themes/planetcstudios/functions.php
@@ -296,26 +296,34 @@ add_action( 'init', 'planetcstudios_menus' );
  * @param array $mce_init TinyMCE styles.
  * @return array TinyMCE styles.
  */
-function planetcstudios_add_classic_editor_non_latin_styles( $mce_init ) {
 
-	$styles = PlanetCStudios_Non_Latin_Languages::get_non_latin_css( 'classic-editor' );
+// function planetcstudios_add_classic_editor_non_latin_styles( $mce_init ) {
+
+	// $styles = PlanetCStudios_Non_Latin_Languages::get_non_latin_css( 'classic-editor' );
 
 	// Return if there are no styles to add.
-	if ( ! $styles ) {
-		return $mce_init;
-	}
 
-	if ( ! isset( $mce_init['content_style'] ) ) {
-		$mce_init['content_style'] = $styles . ' ';
-	} else {
-		$mce_init['content_style'] .= ' ' . $styles . ' ';
-	}
+	// if ( ! $styles ) {
 
-	return $mce_init;
+		// return $mce_init;
 
-}
+	// }
 
-add_filter( 'tiny_mce_before_init', 'planetcstudios_add_classic_editor_non_latin_styles' );
+	// if ( ! isset( $mce_init['content_style'] ) ) {
+
+		// $mce_init['content_style'] = $styles . ' ';
+
+	// } else {
+
+		// $mce_init['content_style'] .= ' ' . $styles . ' ';
+
+	// }
+
+	// return $mce_init;
+
+// }
+
+// add_filter( 'tiny_mce_before_init', 'planetcstudios_add_classic_editor_non_latin_styles' );
 
 /**
  * Block Editor Settings.
