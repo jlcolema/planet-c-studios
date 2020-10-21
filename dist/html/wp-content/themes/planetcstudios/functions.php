953ad3150c06c6553adf2384d18a4dba6daa5c4b
Joshua Coleman <jlcolema@me.com>
2020-10-21 09:10:35 -0500

    Remove non-latin language styles.

--- dist/html/wp-content/themes/planetcstudios/functions.php
+++ dist/html/wp-content/themes/planetcstudios/functions.php
@@ -162,22 +162,6 @@ function planetcstudios_register_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'planetcstudios_register_scripts' );
 
-/** Enqueue non-latin language styles
- *
- * @since Planet C Studios 1.0
- *
- * @return void
- */
-function planetcstudios_non_latin_languages() {
-	$custom_css = PlanetCStudios_Non_Latin_Languages::get_non_latin_css( 'front-end' );
-
-	if ( $custom_css ) {
-		wp_add_inline_style( 'planetcstudios-style', $custom_css );
-	}
-}
-
-add_action( 'wp_enqueue_scripts', 'planetcstudios_non_latin_languages' );
-
 /**
  * Register navigation menus uses wp_nav_menu in one place.
  */
