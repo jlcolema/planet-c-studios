63f6a91f0281d1f208eff3ec23edb75cbe70eddb
Joshua Coleman <jlcolema@me.com>
2020-10-21 09:27:54 -0500

    Remove support for accessible colors for an area.

--- dist/html/wp-content/themes/planetcstudios/functions.php
+++ dist/html/wp-content/themes/planetcstudios/functions.php
@@ -326,45 +326,6 @@ add_action( 'init', 'planetcstudios_menus' );
 // add_filter( 'tiny_mce_before_init', 'planetcstudios_add_classic_editor_non_latin_styles' );
 
 /**
- * Get accessible color for an area.
- *
- * @since Planet C Studios 1.0
- *
- * @param string $area The area we want to get the colors for.
- * @param string $context Can be 'text' or 'accent'.
- * @return string Returns a HEX color.
- */
-function planetcstudios_get_color_for_area( $area = 'content', $context = 'text' ) {
-
-	// Get the value from the theme-mod.
-	$settings = get_theme_mod(
-		'accent_accessible_colors',
-		array(
-			'content'       => array(
-				'text'      => '#000000',
-				'accent'    => '#cd2653',
-				'secondary' => '#6d6d6d',
-				'borders'   => '#dcd7ca',
-			),
-			'header-footer' => array(
-				'text'      => '#000000',
-				'accent'    => '#cd2653',
-				'secondary' => '#6d6d6d',
-				'borders'   => '#dcd7ca',
-			),
-		)
-	);
-
-	// If we have a value return it.
-	if ( isset( $settings[ $area ] ) && isset( $settings[ $area ][ $context ] ) ) {
-		return $settings[ $area ][ $context ];
-	}
-
-	// Return false if the option doesn't exist.
-	return false;
-}
-
-/**
  * Returns an array of variables for the customizer preview.
  *
  * @since Planet C Studios 1.0
