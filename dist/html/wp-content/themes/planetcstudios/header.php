3c014f9ffba38569601edc93e9cc856e84f8ffc0
Joshua Coleman <jlcolema@me.com>
2020-10-24 11:42:50 -0500

    Remove description meta since it will be handled by SEO.

--- dist/html/wp-content/themes/planetcstudios/header.php
+++ dist/html/wp-content/themes/planetcstudios/header.php
@@ -19,8 +19,6 @@
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 
-	<!-- <meta name="description" content=""> -->
-
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<!-- <link rel="manifest" href="site.webmanifest"> -->
