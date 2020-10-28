f5417e4a70ba72cdd8ad0c5392c15cf74546e04c
Joshua Coleman <jlcolema@me.com>
2020-10-28 10:33:47 -0500

    Disable call for web manifest until needed.

--- dist/html/wp-content/themes/planetcstudios/header.php
+++ dist/html/wp-content/themes/planetcstudios/header.php
@@ -21,7 +21,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link rel="manifest" href="site.webmanifest">
+	<!-- <link rel="manifest" href="site.webmanifest"> -->
 
 	<link rel="apple-touch-icon" href="icon.png">
 
