6eb83d4a6c19efc69b9aaeb705b5cfd4a8410609
Joshua Coleman <jlcolema@me.com>
2020-10-31 18:19:41 -0500

    Add support for SVG uploads.

--- dist/html/wp-content/themes/planetcstudios/functions.php
+++ dist/html/wp-content/themes/planetcstudios/functions.php
@@ -463,17 +463,17 @@ add_action( 'init', 'deregister_features' );
 
 // Notes...
 
-// function add_file_types_to_uploads($file_types) {
+function add_file_types_to_uploads($file_types) {
 
-	// $new_filetypes = array();
-	// $new_filetypes['svg'] = 'image/svg+xml';
-	// $file_types = array_merge($file_types, $new_filetypes);
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes);
 
-	// return $file_types;
+	return $file_types;
 
-// }
+}
 
-// add_action('upload_mimes', 'add_file_types_to_uploads');
+add_action('upload_mimes', 'add_file_types_to_uploads');
 
 /**
  * Contact Form 7
