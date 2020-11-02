33004ec6140f5ace84f8d3daf121e0cff1c33c2c
Joshua Coleman <jlcolema@me.com>
2020-11-01 18:45:32 -0600

    Connect Founder to the system.

--- dist/html/wp-content/themes/planetcstudios/inc/about.php
+++ dist/html/wp-content/themes/planetcstudios/inc/about.php
@@ -44,25 +44,69 @@
 
 					<div class="core-team__container core-team__founder">
 
-						<div class="member__item member__item--founder">
+						<?php
 
-							<div class="member__photo">
+							$about_founder = get_field( 's_about_founder' );
 
-								<img src="https://via.placeholder.com/120x120.png?text=Photo+(120x120)" alt="A photo of Michael Carone" class="member__img" />
+						?>
 
-							</div>
+						<?php if ( $about_founder ) : ?>
 
-							<h4 class="member__name">Michael Carone</h4>
+							<?php
 
-							<h5 class="member__role">Founder/Director</h5>
+								// Photo
 
-							<div class="member__bio">
+								$member_photo_attachment_id = get_field( 'member_photo', $about_founder->ID );
 
-								<p>Carone was born in Milwaukee, Wisconsin and raised in Los Angeles, California. With a strong resume of practical production and post-production experience dating back to his teenage years, Carone was hired by Universal Studios immediately after graduating from the University of Southern California (USC) to work at Universal Creative in the media production group. In 2001, Carone founded Planet C Studios to further his career as a Director (DGA) and Producer. Carone recently directed and produced a half-hour comedy pilot, <a href="https://www.imdb.com/title/tt2887794/combined">Sensitive Men</a>, featuring Whoopi Goldberg, French Stewart and Metta World Peace, which is currently being considered for series at a premium cable network.</p>
+								// Size Labels
 
-							</div>
+								$member_photo_size_large = 'photo-large';
+								$member_photo_size_small = 'photo-small';
 
-						</div>						
+								// Image Sizes
+
+								$member_photo_large = wp_get_attachment_image_src( $member_photo_attachment_id, $member_photo_size_large );
+								$member_photo_small = wp_get_attachment_image_src( $member_photo_attachment_id, $member_photo_size_small );
+
+								// Role
+
+								$member_role = get_field( 'member_role', $about_founder->ID );
+
+								// Bio
+
+								$member_bio = get_field( 'member_bio', $about_founder->ID );
+
+							?>
+
+							<div class="member__item member__item--founder">
+
+								<div class="member__photo">
+
+									<?php if ( ! $member_photo_small ) : ?>
+
+										<img src="https://via.placeholder.com/120x120.png?text=Placeholder+Photo" srcset="https://via.placeholder.com/120x120.png?text=Placeholder+Photo 2x" alt="A placeholder photo for <?php echo esc_html( $about_founder->post_title ); ?>" width="120" height="120" decoding="async" loading="lazy" class="member__img member__img--is-placeholder" />
+
+										<?php else : ?>
+
+										<img src="<?php echo $member_photo_large[0]; ?>" srcset="<?php echo $member_photo_x_large[0]; ?> 2x" alt="A photo of <?php echo esc_html( $about_founder->post_title ); ?>" width="120" height="120" decoding="async" loading="lazy" class="member__img" />
+
+									<?php endif; ?>
+
+								</div>
+
+								<h4 class="member__name"><?php echo get_the_title( $about_founder->ID ) ?></h4>
+
+								<h5 class="member__role"><?php echo $member_role; ?></h5>
+
+								<div class="member__bio">
+
+									<?php echo $member_bio; ?>
+
+								</div>
+
+							</div>						
+
+						<?php endif; ?>
 
 					</div>
 
