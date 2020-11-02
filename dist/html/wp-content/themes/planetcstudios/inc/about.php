0dcabeb81ac01ac16836bfc6c3c28623e763d2c0
Joshua Coleman <jlcolema@me.com>
2020-11-01 18:23:00 -0600

    Connect Key Collaborators list to the system.

--- dist/html/wp-content/themes/planetcstudios/inc/about.php
+++ dist/html/wp-content/themes/planetcstudios/inc/about.php
@@ -88,7 +88,7 @@
 
 											// Photo
 
-											$member_photo_attachment_id = get_field( 'member_photo', $project->ID );
+											$member_photo_attachment_id = get_field( 'member_photo', $post->ID );
 
 											// Size Labels
 
@@ -155,233 +155,74 @@
 
 				<div class="members__scroll members__scroll--key-collaborators">
 
-					<ul class="members__list members__list--key-collaborators">
+					<?php
 
-						<li class="member__item member__item--key-collaborators">
+						$key_collaborators = get_field( 's_about_key_collaborators' );
 
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Tom Clancy" class="member__img" />
-							
-							</div>
+					?>
 
-							<h4 class="member__name">Tom Clancy</h4>
+					<?php if ( $key_collaborators ) : ?>
 
-							<h5 class="member__role">Director of Photography</h5>
+						<ul class="members__list members__list--key-collaborators">
 
-							<div class="member__bio">
+							<?php foreach ( $key_collaborators as $post ) : ?>
 
-								<p>Tom Clancey has been the Director of Photography for various projects with Planet C Studios including for the pilot episode of the original television comedy "Sensitive Men" featuring Whoopi Goldberg, Metta World Peace, and French Stewart and for Special Venue live action shoots for Universal Studios Hollywood, such as the Studio Backlot Tour, Transformers, Despicable Me, and Halloween Horror Nights.</p>
-								
-								<p>In addition, Clancey has an extensive list of feature film, television, commercial, special venue, documentary, and music video credits. Clancey's full credits and examples of his work can be found here: <a href="https://tomclancey.com">View Portfolio</a>.</p>
+								<?php setup_postdata( $post ); ?>
 
-							</div>
+								<?php
 
-						</li>
+									// Photo
 
-						<li class="member__item member__item--key-collaborators">
+									$member_photo_attachment_id = get_field( 'member_photo', $post->ID );
 
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Christopher Biggs" class="member__img" />
-							
-							</div>
+									// Size Labels
 
-							<h4 class="member__name">Christopher Biggs</h4>
+									$member_photo_size_large = 'photo-large';
+									$member_photo_size_small = 'photo-small';
 
-							<h5 class="member__role">SFX Supervisor/Digital Artist</h5>
+									// Image Sizes
 
-							<div class="member__bio">
+									$member_photo_large = wp_get_attachment_image_src( $member_photo_attachment_id, $member_photo_size_large );
+									$member_photo_small = wp_get_attachment_image_src( $member_photo_attachment_id, $member_photo_size_small );
 
-								<p>Christopher Biggs is a cross platform "Digital Ronin" with an emphasis in video production for theme parks. Most recently he's been working with Universal Studios Tours. His credits include multiple video projections for Imagineering at Tokyo Disneyland & Disney Seas, Euro Disney Paris, Disney World Florida, and Disneyland, Anaheim - including such favorite attractions as "The Pirates of the Caribbean", "The Haunted Mansion", "The Little Mermaid", and "Space Mountain."</p>
-								
-								<p>Chris and Planet C collaborate often on projection mapping projects including may effects seen in Warner Bros. Horror Made Here attractions.</p>
+								?>
 
-							</div>
+								<li class="member__item member__item--key-collaborators">
 
-						</li>
+									<div class="member__photo">
+											
+										<?php if ( ! $member_photo_small ) : ?>
 
-						<li class="member__item member__item--key-collaborators">
+											<img src="https://via.placeholder.com/60x60.png?text=Placeholder+Photo" srcset="https://via.placeholder.com/120x120.png?text=Placeholder+Photo 2x" alt="A placeholder photo for <?php the_title(); ?>" width="60" height="60" decoding="async" loading="lazy" class="member__img member__img--is-placeholder" />
 
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
+										<?php else : ?>
 
-							<h4 class="member__name">Casey Dodd</h4>
+											<img src="<?php echo $member_photo_small[0]; ?>" srcset="<?php echo $member_photo_large[0]; ?> 2x" alt="A photo of <?php the_title(); ?>" width="60" height="60" decoding="async" loading="lazy" class="member__img" />
 
-							<h5 class="member__role">Artist</h5>
 
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
+										<?php endif; ?>
 
-							</div>
+									</div>
 
-						</li>
+									<h4 class="member__name"><?php the_title(); ?></h4>
 
-						<li class="member__item member__item--key-collaborators">
+									<h5 class="member__role"><?php the_field( 'member_role' ); ?></h5>
 
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
+									<div class="member__bio">
 
-							<h4 class="member__name">Casey Dodd</h4>
+										<?php the_field( 'member_bio' ); ?>
 
-							<h5 class="member__role">Artist</h5>
+									</div>
 
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
+								</li>
 
-							</div>
+							<?php endforeach; ?>
 
-						</li>
+						</ul>
 
-						<li class="member__item member__item--key-collaborators">
+						<?php wp_reset_postdata(); ?>
 
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
-
-							<h4 class="member__name">Casey Dodd</h4>
-
-							<h5 class="member__role">Artist</h5>
-
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
-
-							</div>
-
-						</li>
-
-						<li class="member__item member__item--key-collaborators">
-
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
-
-							<h4 class="member__name">Casey Dodd</h4>
-
-							<h5 class="member__role">Artist</h5>
-
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
-
-							</div>
-
-						</li>
-
-						<li class="member__item member__item--key-collaborators">
-
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
-
-							<h4 class="member__name">Casey Dodd</h4>
-
-							<h5 class="member__role">Artist</h5>
-
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
-
-							</div>
-
-						</li>
-
-						<li class="member__item member__item--key-collaborators">
-
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
-
-							<h4 class="member__name">Casey Dodd</h4>
-
-							<h5 class="member__role">Artist</h5>
-
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
-
-							</div>
-
-						</li>
-
-						<li class="member__item member__item--key-collaborators">
-
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
-
-							<h4 class="member__name">Casey Dodd</h4>
-
-							<h5 class="member__role">Artist</h5>
-
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
-
-							</div>
-
-						</li>
-
-						<li class="member__item member__item--key-collaborators">
-
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
-
-							<h4 class="member__name">Casey Dodd</h4>
-
-							<h5 class="member__role">Artist</h5>
-
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
-
-							</div>
-
-						</li>
-
-						<li class="member__item member__item--key-collaborators">
-
-							<div class="member__photo">
-							
-								<img src="https://via.placeholder.com/60x60.png?text=Photo+(60x60)" alt="A photo of Casey Dodd" class="member__img" />
-							
-							</div>
-
-							<h4 class="member__name">Casey Dodd</h4>
-
-							<h5 class="member__role">Artist</h5>
-
-							<div class="member__bio">
-							
-								<p>Casey Dodd is an established artist that has created animation concepts, character designs, storyboards, attraction concepts and much more for well renown clients including Universal Studios, The Walt Disney Company, Nickelodeon, MTV Productions, and the Landmark Entertainment Group. His work with Planet C Studios spans original cartoon series development (<a href="/work/harry-hare/">Harry Hare</a>), Universal Studios Hollywood Halloween Horror Nights attraction / character conceptualization, and new theme park attraction designs. Some of his extensive work can be found here: <a href="https://www.caseydodd.com">View Portfolio</a>.</p>
-
-							</div>
-
-						</li>
-
-					</ul>
+					<?php endif; ?>
 
 				</div>
 
