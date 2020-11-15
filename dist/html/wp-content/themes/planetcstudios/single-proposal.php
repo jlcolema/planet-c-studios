ff831fab513946ee46836612dbbdad316bb80efb
Joshua Coleman <jlcolema@me.com>
2020-11-15 09:54:14 -0600

    Add markup and styles for Proposal entries and connect to the system.

--- dist/html/wp-content/themes/planetcstudios/single-proposal.php
+++ dist/html/wp-content/themes/planetcstudios/single-proposal.php
@@ -15,235 +15,218 @@
 
 <?php get_header(); ?>
 
-	<div class="projects">
+	<?php if ( post_password_required() ) : ?>
 
-		<div class="projects__nav">
+		<?php echo get_the_password_form(); ?>
 
-			<h1 class="projects__header">Projects (for Proposals)</h1>
+	<?php else : ?>
 
-			<select name="projects-filter" id="" class="projects__filter">
+		<?php
 
-				<option value="">See All</option>
-				<option value="animation">Animation</option>
-				<option value="branding">Branding</option>
-				<option value="comedy">Comedy</option>
-				<option value="concept">Concept</option>
-				<option value="documentary">Documentary</option>
-				<option value="educational">Educational</option>
-				<option value="graphics">Graphics</option>
-				<option value="horror">Horror</option>
-				<option value="montage">Montage</option>
-				<option value="music-video">Music Video</option>
-				<option value="production">Production</option>
-				<option value="projection-mapping">Projection Mapping</option>
-				<option value="promo">Promo</option>
-				<option value="scripted">Scripted</option>
-				<option value="short">Short</option>
-				<option value="special-venue">Special Venue</option>
-				<option value="star-talent">Star Talent</option>
-				<option value="television">Television</option>
-				<option value="visual-effects">Visual Effects</option>
+			// Cover
 
-			</select>
+			$proposal_cover_attachment_id = get_field( 'proposal_cover', $proposal->ID );
+
+			// Size Labels
+
+			$proposal_cover_size_large = 'cover-large';
+			$proposal_cover_size_small = 'cover-small';
+
+			// Image Sizes
+
+			$proposal_cover_large = wp_get_attachment_image_src( $proposal_cover_attachment_id, $proposal_cover_size_large );
+			$proposal_cover_small = wp_get_attachment_image_src( $proposal_cover_attachment_id, $proposal_cover_size_small );
+
+			// Clients
+
+			$proposal_clients = get_field( 'proposal_client' );
+
+			// Title
+
+			$proposal_cover = get_field( 'proposal_cover' );
+
+		?>
+
+		<div class="proposal">
+
+			<div class="proposal__inner-wrap">
+
+				<div class="proposal__overview">
+
+					<h1 class="proposal__s-title"><?php the_title(); ?></h1>
+
+					<div class="proposal__meta">
+
+						<!-- 
+							
+							Two sizes are available:
+								
+							1. cover-large, 380 x 560, for higher density screens.
+							2. cover-small, 190 x 288, the default output.
+
+						-->
+
+						<div class="proposal__s-cover">
+
+							<?php if ( $proposal_cover ) : ?>
+
+								<img src="<?php echo $proposal_cover_small[0]; ?>" srcset="<?php echo $proposal_cover_large[0]; ?> 2x" alt="The cover image for <?php echo $proposal_title; ?>" width="190" height="280" decoding="async" loading="lazy" class="proposal__s-img" />
+
+							<?php else : ?>
+
+								<img src="https://via.placeholder.com/190x280.png?text=Placeholder+Cover" srcset="https://via.placeholder.com/380x560.png?text=Placeholder-Cover 2x" alt="The placeholder cover image for <?php echo $proposal_title; ?>" width="190" height="280" decoding="async" loading="lazy" class="proposal__s-img proposal__s-img--is-placeholder" />
+
+							<?php endif; ?>
+
+						</div>
+
+						<div class="proposal__details">
+
+							<?php if ( get_field( 'proposal_description' ) ) : ?>
+
+								<div class="proposal__description">
+
+									<?php the_field( 'proposal_description' ); ?>
+
+								</div>
+
+							<?php endif; ?>
+
+							<div class="proposal__extra">
+
+								<?php if ( get_field( 'proposal_date' ) ) : ?>
+
+									<div class="proposal__date"><?php the_field( 'proposal_date' ); ?></div>
+
+								<?php endif; ?>
+
+								<?php if ( get_field( 'proposal_location' ) ) : ?>
+
+									<div class="proposal__location"><?php the_field( 'proposal_location' ); ?></div>
+
+								<?php endif; ?>
+
+								<?php if ( $proposal_clients ) : ?>
+
+									<div class="proposal__client">
+
+										<span class="client__label">Client:</span>
+										
+										<ul class="client__list">
+
+											<?php foreach ( $proposal_clients as $proposal_client ) : ?>
+											
+												<?php
+
+													// Title
+
+													$client_title = get_the_title( $proposal_client->ID );
+
+												?>
+
+												<li class="client__item"><?php echo esc_html( $client_title ); ?></li>
+
+											<?php endforeach; ?>
+
+										</ul>
+
+									</div>
+
+								<?php endif; ?>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+				<div class="proposal__projects">
+
+					<!-- Featured Projects -->
+
+					<?php
+
+						$proposal_projects = get_field( 'proposal_projects' );
+
+					?>
+
+					<?php if ( $proposal_projects ) : ?>
+
+						<ul class="proposal-projects__list">
+
+							<?php foreach ( $proposal_projects as $post ) : ?>
+
+								<?php setup_postdata( $post ); ?>
+
+								<?php
+
+									// Cover
+
+									$project_cover_attachment_id = get_field( 'project_cover', $project->ID );
+
+									// Size Labels
+
+									$project_cover_size_large = 'cover-large';
+									$project_cover_size_small = 'cover-small';
+
+									// Image Sizes
+
+									$project_cover_large = wp_get_attachment_image_src( $project_cover_attachment_id, $project_cover_size_large );
+									$project_cover_small = wp_get_attachment_image_src( $project_cover_attachment_id, $project_cover_size_small );
+
+									$project_cover = get_field( 'project_cover' );
+
+								?>
+
+								<li class="proposal-project__item">
+
+									<a href="<?php the_permalink(); ?>" class="proposal-project__link">
+
+										<!-- 
+											
+											Two sizes are available:
+												
+											1. cover-large, 380 x 560, for higher density screens.
+											2. cover-small, 190 x 288, the default output.
+
+										-->
+
+										<div class="proposal-project__cover">
+
+											<?php if ( $project_cover ) : ?>
+
+												<img src="<?php echo $project_cover_small[0]; ?>" srcset="<?php echo $project_cover_large[0]; ?> 2x" alt="The cover image for <?php the_title(); ?>" width="190" height="280" decoding="async" loading="lazy" class="proposal-project__img" />
+
+											<?php else : ?>
+
+												<img src="https://via.placeholder.com/190x280.png?text=Placeholder+Cover" srcset="https://via.placeholder.com/380x560.png?text=Placeholder-Cover 2x" alt="The placeholder cover image for <?php the_title(); ?>" width="190" height="280" decoding="async" loading="lazy" class="proposal-project__img proposal-project__img--is-placeholder" />
+
+											<?php endif; ?>
+						
+										</div>
+
+										<h3 class="proposal-project__title"><?php the_title(); ?></h3>
+
+									</a>
+
+								</li>
+
+							<?php endforeach; ?>
+
+						</ul>
+
+						<?php wp_reset_postdata(); ?>
+
+					<?php endif; ?>
+
+				</div>
+
+			</div>
 
 		</div>
 
-		<ul class="projects__list">
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project"width="200" height="300" decoding="async" loading="lazy"  class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-			<li class="project__item project__item--category-animation">
-
-				<a href="/work/project-01/" class="project__link">
-
-					<div class="project__cover">
-
-						<img src="https://via.placeholder.com/200x300.png?text=Cover+(200x300)" alt="The cover image for Title of Project" width="200" height="300" decoding="async" loading="lazy" class="project__img" />
-
-					</div>
-
-					<h3 class="project__title">Title of Project</h3>
-
-				</a>
-
-			</li>
-
-		</ul>
-
-	</div>
+	<?php endif; ?>
 
 <?php get_footer(); ?>
