866853021042801e854addc236bc6e7b67ec22e9
Joshua Coleman <jlcolema@me.com>
2020-11-28 11:16:50 -0600

    Remove manifest.

--- dist/html/wp-content/themes/planetcstudios/header.php
+++ dist/html/wp-content/themes/planetcstudios/header.php
@@ -21,8 +21,6 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<!-- <link rel="manifest" href="site.webmanifest"> -->
-
 	<link rel="apple-touch-icon" href="icon.png">
 
 	<?php wp_head(); ?>
